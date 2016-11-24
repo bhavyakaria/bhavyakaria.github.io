ef11f7138b029c9602e1724e9ecc67d5f0ca6862
bhavyakaria <bhavyakaria@yahoo.in>
2016-11-24 21:00:01 +0530

    Animated the Social Media icons.

---
+++ main.js
@@ -0,0 +1,55 @@
+function rotate(){
+	TweenMax.to(".fb", 2, {
+			rotation: 360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function rotateb(){
+	TweenMax.to(".fb", 2, {
+			rotation: -360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function trotate(){
+	TweenMax.to(".twitter", 2, {
+			rotation: 360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function trotateb(){
+	TweenMax.to(".twitter", 2, {
+			rotation: -360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function irotate(){
+	TweenMax.to(".insta", 2, {
+			rotation: 360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function irotateb(){
+	TweenMax.to(".insta", 2, {
+			rotation: -360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function lrotate(){
+	TweenMax.to(".link", 2, {
+			rotation: 360,
+			transformOrigin: '50% 50%'
+	});
+}
+
+function lrotateb(){
+	TweenMax.to(".link", 2, {
+			rotation: -360,
+			transformOrigin: '50% 50%'
+	});
+}

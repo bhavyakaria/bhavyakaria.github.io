aaa589894d8b037d01da2c1749ae86f4c2113b1d
bhavyakaria <bhavyakaria@yahoo.in>
2021-08-02 00:28:18 +0530

    chore: will this workd

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   url: 'https://bhavyakaria.github.io',
-  pathPrefix: '/bhavyakaria.github.io',
+  pathPrefix: '/public',
   title: 'Bhavya Karia',
   subtitle: 'Welcome to my den! I blog about web development, android and a lot more.',
   copyright: '© Bhavya Karia 2021',

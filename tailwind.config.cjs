d778386d05a7bba6ac6d3f401da8daabce9bc400
bhavyakaria <bhavyakaria@yahoo.in>
2026-06-07 01:26:45 +0530

    chore: changed font for complete website to aleo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'aleo': ['Aleo', 'serif'],
+				'serif': ['Aleo', 'serif'],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
